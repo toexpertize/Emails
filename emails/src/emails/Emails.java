60b831d4cddcc4be739ef91c7347d00b4958327b
toexpertize <toexpertize@gmail.com>
2024-06-17 08:11:03 +0300

    email app - test3

--- emails/src/emails/Emails.java
+++ emails/src/emails/Emails.java
@@ -16,8 +16,9 @@ public class Emails {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("test");
+        
         System.out.println("test2");
+        
     }
     
 }
