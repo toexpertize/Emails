462de8b60cffefe5438b4a84d8be67ffc359061f
toexpertize <toexpertize@gmail.com>
2024-06-17 13:23:44 +0300

    vs test

--- emails/src/emails/Emails.java
+++ emails/src/emails/Emails.java
@@ -17,7 +17,7 @@ public class Emails {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        System.out.println("test2");
+        System.out.println("test3");
         
     }
     
